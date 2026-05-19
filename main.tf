3d3b302f4699ac8877e16cf82b9d055cb195df55
akshathaa22 <akshathaajagannathan@gmail.com>
2026-05-19 19:03:34 +0530

    good2

--- main.tf
+++ main.tf
@@ -11,15 +11,15 @@ provider "azurerm" {
 	features {}
 }
 
-resource "azurerm_resource_group_name" "demo" {
+resource "azurerm_resource_group" "demo" {
 	name= "rg_demo_devops"
 	location= "Central India"
 }
 
 resource "azurerm_container_registry" "acr_demo" {
 	name= "myuniquedevopsacr"
-	location= "azurerm_resource_group_name.demo.location"
-	resource_group_name= "azurerm_resource_group_name.demo.name"
+	location= "azurerm_resource_group.demo.location"
+	resource_group_name= "azurerm_resource_group.demo.name"
 	sku= "Standard"
 	admin_enabled= false    
 }
