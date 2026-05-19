83fc7727de1cf25e49cfe491add8961d9b121e8b
akshathaa22 <akshathaajagannathan@gmail.com>
2026-05-19 19:43:00 +0530

    good4

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "demo" {
 resource "azurerm_container_registry" "acr_demo" {
 	name= "myuniquedevopsacr"
 	location= "australiacentral"
-	resource_group_name= "azurerm_resource_group.demo.name"
+	resource_group_name= azurerm_resource_group.demo.name
 	sku= "Standard"
 	admin_enabled= false    
 }
