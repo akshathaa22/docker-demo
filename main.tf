491e2cbe122bc859fe024f1835c174657618e640
akshathaa22 <akshathaajagannathan@gmail.com>
2026-05-20 20:02:06 +0530

    azure app

--- main.tf
+++ main.tf
@@ -23,3 +23,46 @@ resource "azurerm_container_registry" "acr_demo" {
 	sku= "Standard"
 	admin_enabled= false    
 }
+
+resource "azurerm_container_app_environment" "demo" {
+  name                = "container-env-demo"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+}
+
+resource "azurerm_container_app" "demo" {
+  name                         = "nginx-containerapp"
+  container_app_environment_id = azurerm_container_app_environment.demo.id
+  resource_group_name          = azurerm_resource_group.demo.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "nginx"
+      image  = "myuniquedevopsacr.azurecr.io/my-nginx-image:v1"
+      cpu    = 0.5
+      memory = "1Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 80
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  registry {
+    server               = "myuniquedevopsacr.azurecr.io"
+    username             = azurerm_container_registry.demo.admin_username
+    password_secret_name = "acr-password"
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.demo.admin_password
+  }
+}
