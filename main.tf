2710aff7cb311e753b35a6569a20144c757b27b6
akshathaa22 <akshathaajagannathan@gmail.com>
2026-05-21 16:27:25 +0530

    commiting1

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_container_registry" "acr_demo" {
 	location= "australiacentral"
 	resource_group_name= azurerm_resource_group.demo.name
 	sku= "Standard"
-	admin_enabled= false    
+	admin_enabled= true
 }
 
 resource "azurerm_container_app_environment" "demo" {
